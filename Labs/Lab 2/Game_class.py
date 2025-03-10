4869bca82b562863552e2f4dbcf810906034030e
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-03-10 22:20:02 +0200

    Update Game_class.py

--- Labs/Lab 2/Game_class.py
+++ Labs/Lab 2/Game_class.py
@@ -26,7 +26,24 @@ def getButtons():
     ...
 
 initial_state=getButtons() #get the initial states of the buttons
-
+points=0
 while True:
-    pressed=getButtons()#get the buttons
-    
+    print("Points",points)
+    ... #randomly generate a sequence
+    ... #play sequence
+    #wait for response
+    response=False
+    user_sequence=[]
+    while not response:
+        pressed=getButtons()#get the buttons
+        ... #gather each button in the user sequence
+        #check that the button pressed is the next one in the sequence
+        if ...: #if button is pressed
+            if ....: # if the button is wrong
+                print("Incorrect")
+                response=True
+            elif ...: #it is correct and sequence is of the same length
+                print("Correct, generating new")
+                response=True
+                points+=1
+                
